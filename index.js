356c5ac43ff252c4e775be5d921aa1d02a5a7e07
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2024-08-20 21:17:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+import puppeteer from "puppeteer-core";
+import { exec } from "node:child_process";
+import util from "node:util";
+import { PuppeteerScreenRecorder } from 'puppeteer-screen-recorder';
+import fs from "node:fs";
+import path from "node:path";
+import stream from "node:stream";
+
+const __dirname = path.resolve();
+
+function PassThrough() {
+  const s = new fs.createWriteStream('stream.mp4')
+  return s;
+}
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function main() {
+
+  const { stdout: chromiumPath } = await util.promisify(exec)("which chromium")
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    executablePath: chromiumPath.trim()
+  });
+
+const page = await browser.newPage();
+  const recorder = new PuppeteerScreenRecorder(page);
+  const pipeStream = new PassThrough();
+  await recorder.startStream(pipeStream);
+  await page.goto('https://orteil.dashnet.org/cookieclicker/');
+  await page.setViewport({width: 1080, height: 1024});
+  await page.click(`a[data-cc-event="click:dismiss"]`)
+  await page.mouse.click(540, 350, { button: 'left' })
+  await sleep(3000)
+  await recorder.stop();
+  await browser.close();
+}
+
+main()
