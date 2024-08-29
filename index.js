1a39041052a750871dcc64a051aa36067110839e
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2024-08-29 23:21:37 +0000

    updating

--- index.js
+++ index.js
@@ -8,6 +8,55 @@ import fs from "node:fs";
 import path from "node:path";
 import stream from "node:stream";
 import readline from "node:readline";
+import {
+  Client,
+  GatewayIntentBits,
+  Collection,
+  Events,
+  Activity,
+  SlashCommandBuilder,
+  Partials,
+  PermissionsBitField,
+  RoleFlagsBitField,
+  RoleManager,
+  ChannelType,
+} from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildWebhooks,
+  ],
+  partials: [Partials.Channel, Partials.Message, Partials.User],
+});
+client.login(process.env.token);
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on("ready", () => {
+  const commands = [
+    new SlashCommandBuilder()
+      .setName("ping")
+      .setDescription("Replies with pong!"),
+    new SlashCommandBuilder()
+      .setName("screenshot")
+      .setDescription("sends a screenshot of the current page"),
+    new SlashCommandBuilder()
+      .setName("record")
+      .setDescription("record a screen")
+      .addChannelOption((option) => {
+        return option
+          .setName("channel")
+          .setDescription("The channel to record to")
+          .setRequired(true);
+      }),
+  ];
+  client.application.commands.set(commands);
+});
 
 const __dirname = path.resolve();
 
@@ -48,10 +97,10 @@ async function getCookies(page) {
 }
 async function getPrices(page) {
   const prices = await page.evaluate(() => {
-    let Prices = '';
+    let Prices = "";
     for (var i in Game.Objects) {
-      var me=Game.Objects[i];
-      let output = `${me.name}: ${me.price}\n`
+      var me = Game.Objects[i];
+      let output = `${me.name}: ${me.price}\n`;
       Prices += output;
     }
     return Prices;
@@ -65,20 +114,22 @@ async function buyBuilding(page, name) {
     } else {
       return -1;
     }
-  }, name)
-  if (buildingId>-1) {await page.click(`#productName${buildingId}`)} else {
-    console.log("invalid building name")
-  };
+  }, name);
+  if (buildingId > -1) {
+    await page.click(`#productName${buildingId}`);
+  } else {
+    console.log("invalid building name");
+  }
 }
 async function save(page) {
-  clearFile('save.txt');
+  clearFile("save.txt");
   const save = await page.evaluate(() => {
     return Game.WriteSave(1);
-  })
-  writeFile('save.txt', save);
+  });
+  writeFile("save.txt", save);
 }
 async function load(page) {
-  const save = await getFile('save.txt');
+  const save = await getFile("save.txt");
   await page.evaluate((save) => {
     Game.ImportSaveCode(save);
   }, save);
@@ -93,29 +144,35 @@ async function getPricesAsArray(page) {
   const prices = await page.evaluate(() => {
     let Prices = [];
     for (var i in Game.Objects) {
-      var me=Game.Objects[i];
-      let output = {name: me.name, price: me.price}
+      var me = Game.Objects[i];
+      let output = { name: me.name, price: me.price };
       Prices += output;
     }
     return Prices;
   });
-  return prices
+  return prices;
 }
 async function calcBuyingWorth(page) {
   let worth = await page.evaluate(() => {
     let Worth = [];
-    let baseBuilding = Game.Objects['Cursor'];
+    let baseBuilding = Game.Objects["Cursor"];
     for (var i in Game.Objects) {
-      var me=Game.Objects[i];
-      let price = baseBuilding.basePrice*1.15**(baseBuilding.amount+1);
-      let outputWorth = (price*me.cps(me))/(baseBuilding.cps(baseBuilding)*me.basePrice)
-      let output = {name: me.name, price: me.price, worth: outputWorth, cps: me.cps(me)};
+      var me = Game.Objects[i];
+      let price = baseBuilding.basePrice * 1.15 ** (baseBuilding.amount + 1);
+      let outputWorth =
+        (price * me.cps(me)) / (baseBuilding.cps(baseBuilding) * me.basePrice);
+      let output = {
+        name: me.name,
+        price: me.price,
+        worth: outputWorth,
+        cps: me.cps(me),
+      };
       Worth.push(output);
     }
-    return Worth
+    return Worth;
   });
   return worth;
-};
+}
 async function getBuildingsToBuy(page) {
   let totalCookiesEarned = await page.evaluate(() => {
     return Game.cookiesEarned;
@@ -124,7 +181,7 @@ async function getBuildingsToBuy(page) {
   let worth2 = [];
   for (let i = 0; i < worth.length; i++) {
     let me = worth[i];
-    if (me.price<totalCookiesEarned) {
+    if (me.price < totalCookiesEarned) {
       worth2.push(me);
     }
   }
@@ -132,17 +189,20 @@ async function getBuildingsToBuy(page) {
   let need = [];
   for (let i = 0; i < worth.length; i++) {
     let me = worth[i];
-    let output = round(Math.log(me.worth)/Math.log(1.15));
+    let output = round(Math.log(me.worth) / Math.log(1.15));
     let buildingAmountNeeded = Math.floor(output);
     let buildingAmount = await page.evaluate((name) => {
       if (Game.Objects[name]) {
         return Game.Objects[name].amount;
       } else {
         return -1;
-      };
+      }
     }, me.name);
-    if (buildingAmountNeeded-buildingAmount>0) {
-      need.push({name: me.name, amount: buildingAmountNeeded-buildingAmount});
+    if (buildingAmountNeeded - buildingAmount > 0) {
+      need.push({
+        name: me.name,
+        amount: buildingAmountNeeded - buildingAmount,
+      });
     }
   }
 }
@@ -174,9 +234,10 @@ async function main() {
   console.log("Ready for control");
   rl.on("line", async (input) => {
     if (input === "stop") {
-      await save(page)
+      await save(page);
       await recorder.stop();
       await browser.close();
+      await client.destroy();
       rl.close();
     }
     if (input.toLowerCase() === "runcommand") {
@@ -199,29 +260,40 @@ async function main() {
           console.log(cookies);
         } else if (command.toLowerCase() === "getprices") {
           const prices = await getPrices(page);
-          writeFile(
-            "copyCommands.txt",
-            `let prices = await getPrices(page)\n`,
-          );
+          writeFile("copyCommands.txt", `let prices = await getPrices(page)\n`);
           console.log(prices);
         } else if (command.toLowerCase() === "buybuilding") {
           rl.question(`What building? `, async (name) => {
             await buyBuilding(page, name);
-            writeFile(
-              "copyCommands.txt",
-              `await buyBuilding(page, ${name})\n`,
-            );
+            writeFile("copyCommands.txt", `await buyBuilding(page, ${name})\n`);
             console.log("Done");
           });
         } else if (command.toLowerCase() === "getcps") {
           const cps = await getCps(page);
-          writeFile(
-            "copyCommands.txt",
-            `let cps = await getCps(page)\n`,
-          );
+          writeFile("copyCommands.txt", `let cps = await getCps(page)\n`);
           console.log(cps);
         }
       });
+    }
+  });
+  client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    if (interaction.commandName === "ping") {
+      await interaction.reply("Pong! " + client.ws.ping);
+    } else if (interaction.commandName === "screenshot") {
+      await interaction.reply("Screenshotting...");
+      await page.screenshot({
+        type: "png",
+        path: "screenshot.png",
+        fullPage: true,
+      });
+      const screenshot = fs.readFileSync("screenshot.png");
+      await interaction.editReply({
+        content: "Screenshotted!",
+        files: [screenshot],
+      });
+    } else if (interaction.commandName === "record") {
+      await interaction.reply("Recording...");
     }
   });
 }
